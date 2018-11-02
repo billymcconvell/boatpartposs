0f20e575927bdcfbd9624dc7490188b1d558f582
billymcconvell <43562809+billymcconvell@users.noreply.github.com>
2018-11-02 15:42:34 +1100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1p-eq51TfVgm7zJ2xUaLbVikshOuXI26cGtJvlsNC1G4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bak6hL8EQ7bXfrqpIaxJ9b56Er4-ruusatJYwWrunvA/edit#gid=0';
